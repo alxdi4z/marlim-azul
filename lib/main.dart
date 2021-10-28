dff14cf8ba10df620cdce0d45971f02a9122d92e
Alex Dias <alexdiasplay@gmail.com>
2021-10-28 09:28:22 -0300

    Ajustes no main e no gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,11 @@ class MyApp extends StatelessWidget {
         if (uri.pathSegments.isNotEmpty) {
           String route = "/${uri.pathSegments.first}";
 
-          return MaterialPageRoute(
-              builder: (context) => pageList.firstWhere(
-                  (page) => page.pageInfo.path == route,
-                  orElse: () => pageList
-                      .firstWhere((page) => page.pageInfo.path == "/404")));
+          Widget pageToGo = pageList.firstWhere(
+              (page) => page.pageInfo.path == route,
+              orElse: () =>
+                  pageList.firstWhere((page) => page.pageInfo.path == "/404"));
+          return MaterialPageRoute(builder: (context) => pageToGo);
         }
       },
       home: homePage,
