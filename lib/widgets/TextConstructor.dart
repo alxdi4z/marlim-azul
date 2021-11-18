ad21e369c88b1956e3aabec10135ce52bc72a2af
Alex <alexdiasplay@gmail.com>
2021-11-17 23:54:13 -0300

    Mudança no subtítulo

--- lib/widgets/TextConstructor.dart
+++ lib/widgets/TextConstructor.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:marlinazul_frontend/constants.dart';
 
 class TextConstructor {
-  static Widget MainTitle(String text, Color color, EdgeInsetsGeometry padding) => Container(
+  static Widget MainTitle(
+          String text, Color color, EdgeInsetsGeometry padding) =>
+      Container(
         padding: padding,
         child: Center(
           child: Text(
@@ -15,33 +17,26 @@ class TextConstructor {
         ),
       );
 
-  static Widget Subtitle(List<TextSpan> listText, Color color, EdgeInsetsGeometry padding) => Container(
-      padding: padding,
-      alignment: Alignment.topLeft,
-      child: Wrap(
-        direction: Axis.horizontal,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        alignment: WrapAlignment.start,
-        children: [
-          CircleAvatar(
-            radius: 3.5,
-            backgroundColor: color,
-          ),
-          const SizedBox(width: 5,),
-          RichText(
+  static Widget Subtitle(
+          List<TextSpan> listText, Color color, EdgeInsetsGeometry padding) =>
+      Container(
+          padding: padding,
+          alignment: Alignment.topLeft,
+          child: RichText(
               textAlign: TextAlign.start,
               text: TextSpan(
                   style: TextStyle(
                       color: color, fontSize: fontSize * 1.2, height: 1.5),
-                  children: listText))
-        ],
-      ));
+                  children: listText)));
 
-  static Widget Paragraph(List<TextSpan> listText, Color color, EdgeInsetsGeometry padding) => Container(
-      padding: padding,
-      child: RichText(
-          textAlign: TextAlign.start,
-          text: TextSpan(
-              style: TextStyle(color: color, fontSize: fontSize, height: 1.5),
-              children: listText)));
+  static Widget Paragraph(
+          List<TextSpan> listText, Color color, EdgeInsetsGeometry padding) =>
+      Container(
+          padding: padding,
+          child: RichText(
+              textAlign: TextAlign.start,
+              text: TextSpan(
+                  style:
+                      TextStyle(color: color, fontSize: fontSize, height: 1.5),
+                  children: listText)));
 }
